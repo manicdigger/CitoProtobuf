747019924bcf4eead32a8468459f4e932c382a5d
exe <exeuser1@gmail.com>
2015-01-27 12:42:06 +0100

    JavaScript

--- CodeGenerator/ProtocolParser/ProtocolParser.cs
+++ CodeGenerator/ProtocolParser/ProtocolParser.cs
@@ -828,6 +828,40 @@ public class ProtoPlatform
             b = Encoding.UTF8.GetBytes(s);
             
         }
+#elif JS
+        native
+        {
+        // http://stackoverflow.com/a/18729931
+	var str = s;
+    var utf8 = [];
+    for (var i=0; i < str.length; i++) {
+        var charcode = str.charCodeAt(i);
+        if (charcode < 0x80) utf8.push(charcode);
+        else if (charcode < 0x800) {
+            utf8.push(0xc0 | (charcode >> 6), 
+                      0x80 | (charcode & 0x3f));
+        }
+        else if (charcode < 0xd800 || charcode >= 0xe000) {
+            utf8.push(0xe0 | (charcode >> 12), 
+                      0x80 | ((charcode>>6) & 0x3f), 
+                      0x80 | (charcode & 0x3f));
+        }
+        // surrogate pair
+        else {
+            i++;
+            // UTF-16 encodes 0x10000-0x10FFFF by
+            // subtracting 0x10000 and splitting the
+            // 20 bits of 0x0-0xFFFFF into two halves
+            charcode = 0x10000 + (((charcode & 0x3ff)<<10)
+                      | (str.charCodeAt(i) & 0x3ff))
+            utf8.push(0xf0 | (charcode >>18), 
+                      0x80 | ((charcode>>12) & 0x3f), 
+                      0x80 | ((charcode>>6) & 0x3f), 
+                      0x80 | (charcode & 0x3f));
+        }
+    }
+        b = utf8;
+        }
 #elif JAVA
         native
         {
@@ -857,6 +891,18 @@ public class ProtoPlatform
         native
         {
             s = Encoding.UTF8.GetString(bytes);
+        }
+#elif JS
+        native
+        {
+            var arr = new Uint8Array(bytes.length);
+	        for(var i = 0; i < bytes.length;i++)
+	        {
+		        arr[i] = bytes[i];
+	        }
+	        var encodedString = String.fromCharCode.apply(null, arr);
+	        var decodedString = decodeURIComponent(escape(encodedString));
+	        s = decodedString;
         }
 #elif JAVA
         native
@@ -889,6 +935,11 @@ public class ProtoPlatform
             len = a.Length;
         }
 #elif JAVA
+        native
+        {
+            len = a.length;
+        }
+#elif JS
         native
         {
             len = a.length;
